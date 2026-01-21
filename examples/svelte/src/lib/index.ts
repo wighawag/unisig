94bb6074435a9b501495cda2d566e7ea52461e84
wighawag <wighawag@gmail.com>
2026-01-21 14:46:43 +0000

    Revert "wip: break web demo"
    This reverts commit 0b238e78a199cb8a8da23a00d46825d9b8ba6d91.

--- examples/svelte/src/lib/index.ts
+++
@@ -1,6 +0,0 @@
-import {createAdapterBundle} from 'unisig';
-
-export type {Tracker} from 'unisig';
-import adapter from '@unisig/svelte';
-
-export const {effect, createTracker} = createAdapterBundle(adapter);
