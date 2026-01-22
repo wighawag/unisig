e70135441effda496f320943c015c0d0604c9f80
wighawag <wighawag@gmail.com>
2026-01-22 11:44:09 +0000

    fix vitest solid issue

--- integration-tests/solidjs/vite.config.ts
+++ integration-tests/solidjs/vite.config.ts
@@ -1,6 +1,8 @@
 import {defineConfig} from 'vitest/config';
+import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
+	plugins: [solidPlugin()],
 	test: {
 		include: ['test/**/*.spec.ts'],
 		environment: 'jsdom',
